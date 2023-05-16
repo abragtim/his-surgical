574882cfd306868e3a3434ae92e85f8f850364aa
abragtim <timurkatime@gmail.com>
2023-05-16 19:05:59 +0200

    Init app.

---
+++ src/main/java/his/department/surgical/SurgicalApplication.java
@@ -0,0 +1,13 @@
+package his.department.surgical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurgicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurgicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/his/department/surgical/SurgicalApplicationTests.java
@@ -0,0 +1,13 @@
+package his.department.surgical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurgicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
